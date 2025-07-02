2fe8f9f9bb997e706922e86d24cf7aff5b4fdd32
LizandroDev1 <00019025@uca.edu.sv>
2025-07-01 18:50:04 -0600

    historia.cpp

---
+++ historia.cpp
@@ -0,0 +1,33 @@
+#include "historia.h"
+#include "utils.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void introduccion() {
+    clearScreen();
+    imprimirLento(" HISTORIA DE CHEPE DUTY\n", 40);
+    pause(1000);
+    imprimirLento("Chepe Buquele, líder de la guerrilla de Apopa, ha sido acusado injustamente...", 30);
+    pause(2000);
+    imprimirLento("El presidente Cristiani lo culpa de una masacre contra obreros.", 30);
+    pause(2000);
+    imprimirLento("Ahora, Chepe debe escapar, recuperar pruebas y limpiar su nombre.", 30);
+    pause(2000);
+    cout << "\nPresiona una tecla para comenzar...\n";
+    getch();
+    clearScreen();
+}
+
+void finalExitoso() {
+    clearScreen();
+    imprimirLento("🎉 ¡Felicidades!", 40);
+    imprimirLento("Chepe logró cruzar el campo minado y encontró los documentos que prueban su inocencia.", 30);
+    imprimirLento("El pueblo conoce la verdad. ¡La justicia ha triunfado!", 30);
+    pause(3000);
+}
+
+void finalFallido(const string& razon) {
+    gameOver(("Fallaste: " + razon).c_str());
+}
