f105ea5eca36801b2897d559920393f776adae1b
LizandroDev1 <00019025@uca.edu.sv>
2025-07-01 20:46:18 -0600

    nivel3.h

---
+++ nivel3.cpp
@@ -0,0 +1,45 @@
+#include "nivel3.h"
+#include "utils.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+
+using namespace std;
+
+bool nivel3() {
+    clearScreen();
+    imprimirLento(" NIVEL 3: Duelo de Cartas\n", 40);
+    pause(1000);
+    imprimirLento("Para probar su lealtad, Chepe debe vencer al comandante en un duelo de cartas.", 30);
+    pause(1500);
+    imprimirLento("Cada jugador saca una carta (1 al 13). El más alto gana 2 de 3 rondas.", 30);
+    pause(1500);
+    cout << "\nPresiona una tecla para comenzar el duelo...\n";
+    getch();
+    clearScreen();
+
+    srand(time(0));
+    int victorias = 0;
+
+    for (int ronda = 1; ronda <= 3; ronda++) {
+        int chepe = rand() % 13 + 1;
+        int enemigo = rand() % 13 + 1;
+
+        cout << "Ronda " << ronda << ": Chepe [" << chepe << "] vs Comandante [" << enemigo << "]\n";
+        if (chepe > enemigo) {
+            cout << " Chepe gana la ronda.\n";
+            victorias++;
+        } else {
+            cout << " Chepe pierde la ronda.\n";
+        }
+        pause(1000);
+    }
+
+    if (victorias >= 2) {
+        mensajeFinal("🃏 Has ganado el duelo de cartas.");
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ nivel3.h
@@ -0,0 +1,6 @@
+#ifndef NIVEL3_H
+#define NIVEL3_H
+
+bool nivel3();
+
+#endif
