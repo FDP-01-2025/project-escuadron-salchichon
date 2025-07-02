a5e7ca6c65bd5f3c7084fa4de78503c68b912861
LizandroDev1 <00019025@uca.edu.sv>
2025-07-01 18:49:50 -0600

    historia.h

---
+++ historia.h
@@ -0,0 +1,10 @@
+#ifndef HISTORIA_H
+#define HISTORIA_H
+
+#include <string>  //  Necesario para usar std::string
+
+void introduccion();
+void finalExitoso();
+void finalFallido(const std::string& razon);
+
+#endif
